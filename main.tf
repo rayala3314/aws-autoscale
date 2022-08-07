73f586fd454cfdb3b17eaeb9c049bd8d5f9cdc7d
Raymundo Ayala <rayala19@csub.edu>
2022-08-07 15:58:31 -0700

    test action

--- main.tf
+++ main.tf
@@ -9,28 +9,11 @@ provider "aws" {
   version = "~> 2.36.0"
 }
 
-# Build the VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.1.0.0/16"
-  instance_tenancy     = "default"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
   tags = {
-    Name      = "Vpc"
-    Terraform = "true"
-  }
-}
-# Build route table 1
-resource "aws_route_table" "route_table1" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "RouteTable1"
-    Terraform = "true"
-  }
-}
-# Build route table 2
-resource "aws_route_table" "route_table2" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "RouteTable2"
-    Terraform = "true"
+    Name = "main"
   }
 }
