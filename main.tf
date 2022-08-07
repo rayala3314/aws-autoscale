72d72468eca83a73ccf6d3f5e4b96d9e741b5369
Raymundo Ayala <rayala19@csub.edu>
2022-08-07 15:56:15 -0700

    test actions

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 # Require TF version to be same as or greater than 0.12.13
-
-
 terraform {
   required_version = ">=0.12.13"
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-west-1"
   version = "~> 2.36.0"
 }
 
