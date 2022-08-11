7d4a532004fae432918cdff1aa04101de6dbfde5
Raymundo Ayala <rayala19@csub.edu>
2022-08-11 14:32:21 -0700

    Testing vpc and subnet

--- main.tf
+++ main.tf
@@ -17,3 +17,13 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name        = "private-subnet"
+  }
+}
