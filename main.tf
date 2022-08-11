8317cc13b6e489226f8f143302b6150c4fa4148f
Raymundo Ayala <rayala19@csub.edu>
2022-08-11 14:40:34 -0700

    Testing terraform

--- main.tf
+++ main.tf
@@ -17,3 +17,13 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/20"
+  availability_zone       = "us-west-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name        = "private_subnet"
+  }
+}
