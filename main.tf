31a38213d6f685c627ae116c5f08e3d5fca28766
Raymundo Ayala <rayala19@csub.edu>
2022-08-12 09:07:36 -0700

    Testing actions

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
-  required_version = ">=0.12.13"
+  required_providers{
+    aws = {
+      source = "hasicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
 provider "aws" {
   region  = "us-west-1"
-  version = "~> 2.36.0"
 }
 
 resource "aws_vpc" "main" {
@@ -16,4 +20,12 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "main"
   }
+}
+
+#Create a private subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.1.0.0/20"
+  availability_zone = "us-west-1a"
+  
 }
