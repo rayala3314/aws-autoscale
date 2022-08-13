150ecb8d0a351ce86d869133e1ca1935ad832b5d
Raymundo Ayala <rayala19@csub.edu>
2022-08-13 12:56:09 -0700

    Test

--- iam.tf
+++ iam.tf
@@ -9,10 +9,3 @@ resource "aws_iam_role_policy_attachment" "test_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_ssm_activation" "foo" {
-  name               = "test_ssm_activation"
-  description        = "Test"
-  iam_role           = aws_iam_role.ssm-user.id
-  registration_limit = "5"
-  depends_on         = [aws_iam_role_policy_attachment.test_attach]
-}
