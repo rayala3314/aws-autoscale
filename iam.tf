e369e0c154fdb2ed005e479e040f30464a26dcd5
Raymundo Ayala <rayala19@csub.edu>
2022-08-17 11:40:06 -0700

    Test SSM

--- iam.tf
+++ iam.tf
@@ -18,10 +18,25 @@ resource "aws_iam_role_policy_attachment" "test_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = aws_iam_role.test_role.name
+}
+
 resource "aws_ssm_activation" "foo" {
   name               = "test_ssm_activation"
   description        = "Test"
   iam_role           = aws_iam_role.test_role.id
   registration_limit = "5"
   depends_on         = [aws_iam_role_policy_attachment.test_attach]
+}
+
+resource "aws_launch_template" "ssm_testing" {
+  name                   = "ssm_test"
+  key_name               = "Ray"
+  image_id               = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.tribehealth_platform.id]
+  iam_instance_profile   = aws_iam_instance_profile.test_profile.name
+  subnet_id              = "${aws_subnet.testapp_private_subnet.id}"
 }
