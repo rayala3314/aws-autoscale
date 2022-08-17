8ece54e13a78e3fbc7f77d6833290c66d4a31670
Raymundo Ayala <rayala19@csub.edu>
2022-08-17 14:48:50 -0700

    Test SSM

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "aws_instance" "web" {
   subnet_id              = aws_subnet.testapp_private_subnet.*.id[count.index]
 }
 /*
---1
+--1a
 data "aws_ami" "ami" {
   most_recent = true
   owners      = ["amazon"]
