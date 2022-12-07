dfc9a02a74f29fd65178e3ca441624338a38e6bf
Raymundo Ayala <rayala19@csub.edu>
2022-12-07 14:01:40 -0800

    tst
    tst

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,23 +1,20 @@
-resource "aws_iam_role" "test_role_prod" {
-  name = "test_role_prod"
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy_prod"
+  path        = "/"
+  description = "My test policy from prod"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
       },
     ]
   })
-
-  tags = {
-    tag-key = "tag-value"
-  }
 }
