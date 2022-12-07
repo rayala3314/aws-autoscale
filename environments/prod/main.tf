3b01aa9e44fd81124a272ffa1e8eda6556823652
Raymundo Ayala <rayala19@csub.edu>
2022-12-07 13:53:51 -0800

    tst
    tst

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,6 +1,23 @@
-resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    count = "${var.number_of_instances}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.ami_key_pair_name}"
-} 
+resource "aws_iam_role" "test_role_prod" {
+  name = "test_role_prod"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -1,32 +1,3 @@
 variable "region" {
   default = "us-west-1"
 }
-
-variable "instance_name" {
-        description = "Name of the instance to be created"
-        default = "awsbuilder-demo-prod"
-}
-
-variable "instance_type" {
-        default = "t2.micro"
-}
-
-variable "subnet_id" {
-        description = "The VPC subnet the instance(s) will be created in"
-        default = "subnet-07ebbe60"
-}
-
-variable "ami_id" {
-        description = "The AMI to use"
-        default = "ami-0f5e8a042c8bfcd5e"
-}
-
-variable "number_of_instances" {
-        description = "number of instances to be created"
-        default = 1
-}
-
-
-variable "ami_key_pair_name" {
-        default = "tomcat"
-}
