0514b46083a10448fb91a3a57fbad74ce6736e1d
Raymundo Ayala <rayala19@csub.edu>
2022-12-07 13:39:06 -0800

    tst
    tst

--- environments/prod/providers.tf
+++ environments/prod/providers.tf
@@ -1,8 +1,8 @@
 # Provider configuration
 provider "aws" {
   region  = var.region
-  access_key = AKIASF7KMHMKWCOGINVU
-  secret_key = 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
+  access_key = "AKIASF7KMHMKWCOGINVU"
+  secret_key = "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"
   version = "~> 3.0"
   profile = "testapp"
 }
