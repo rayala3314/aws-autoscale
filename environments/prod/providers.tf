bd0775ea145283cc652beb75edd5aaa393be5972
Raymundo Ayala <rayala19@csub.edu>
2022-12-07 13:37:48 -0800

    tst
    test

--- environments/prod/providers.tf
+++ environments/prod/providers.tf
@@ -1,8 +1,8 @@
 # Provider configuration
 provider "aws" {
   region  = var.region
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = AKIASF7KMHMKWCOGINVU
+  secret_key = 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
   version = "~> 3.0"
   profile = "testapp"
 }
--- environments/prod/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-access_key = AKIASF7KMHMKWCOGINVU
-secret_key = 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
